19690f7bda2039f63d5ea8038add0a2aada8face
masperverso <93467936+masperverso@users.noreply.github.com>
2023-04-19 11:30:34 +0200

    Update conceptoseconomicos.js

--- conceptoseconomicos.js
+++ conceptoseconomicos.js
@@ -10,12 +10,13 @@ var dietadesayuno = 2.19;
 var transportemadrugue=8.14;
 const plusasistencia=62.82;
 
-//cambio rago salarial por sus 3 componentes
+//cambio rango salarial por sus 3 componentes
 sueldobase=[463.27,490.52,517.16];
 primaproductividad=[115.82,122.63,129.44];
 complementotransitorio=[449.28,475.70,502.13];
 
 horabase=[2.224784,2.355654,2.486525];
+trieniohorabase=[0,0.146124,0.156964,0.154721];
 const plusadicional=61.13;
 const complementodsa=19.82;
 const plusarea=101.21;

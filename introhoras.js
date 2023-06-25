f16e928928fbc5b4b04514e528a257b1f40470e5
masperverso <93467936+masperverso@users.noreply.github.com>
2023-06-25 11:58:35 +0200

    Add files via upload

--- introhoras.js
+++ introhoras.js
@@ -17,9 +17,9 @@ const selectedTimefinal = flatpickr(timepickerfinal, {
   minuteIncrement: 15, // Establecer incremento de minutos a 15
 });
 
-const timepickerinicio2 = document.getElementById("timepickerinicio2");
+/*const timepickerinicio2 = document.getElementById("timepickerinicio2");
 const selectedTimeinicio2 = flatpickr(timepickerinicio2, {
-  enableTime: false,
+  enableTime: true,
   noCalendar: true,
   dateFormat: "H:i",
   time_24hr: true,
@@ -30,13 +30,13 @@ const selectedTimeinicio2 = flatpickr(timepickerinicio2, {
 
 const timepickerfinal2 = document.getElementById("timepickerfinal2");
 const selectedTimefinal2 = flatpickr(timepickerfinal2, {
-  enableTime: false,
+  enableTime: true,
   noCalendar: true,
   dateFormat: "H:i",
   time_24hr: true,
   minuteIncrement: 15, // Establecer incremento de minutos a 15
   defaultDate: "00:00", // Establece el valor inicial en "0:00"
-});
+});*/
 
 
 

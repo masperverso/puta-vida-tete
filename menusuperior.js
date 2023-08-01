f3af55a80f12c19bd1e5b97fe201e4b3d9a6f336
masperverso <93467936+masperverso@users.noreply.github.com>
2023-08-01 21:09:54 +0200

    Add files via upload

---
+++ menusuperior.js
@@ -0,0 +1,13 @@
+function toggleDiv(divId) {
+    var div = document.getElementById(divId);
+
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+
+function toggleColor(img) {
+    img.classList.toggle("active");
+}
